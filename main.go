e0afb556de6508e7772d602750c99a50c12ee994
shivamkarn <shivamkarn767@gmail.com>
2026-01-30 20:13:46 +0545

    Added for loop for continuous execution

--- main.go
+++ main.go
@@ -7,32 +7,48 @@ import (
 	"strings"
 )
 
-func main()  {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Enter Your note : ")
+func main() {
+	for {
+		inputReader := bufio.NewReader(os.Stdin)
+		fmt.Println("Add a new note ? (y/n) : ")
 
-	noteContent , err := reader.ReadString('\n')
+		userInput, _ := inputReader.ReadString('\n')
 
-	noteContent  =strings.TrimSpace(noteContent)
+		userInput = strings.TrimSpace(userInput)
 
-	if err != nil{
-		fmt.Println("Error occured while reading input")
+		if userInput == "n" {
+			fmt.Println("Ok GoodBye!")
+			break
+		} else {
+			reader := bufio.NewReader(os.Stdin)
+
+			fmt.Println("Enter Your note : ")
+
+			noteContent, err := reader.ReadString('\n')
+
+			noteContent = strings.TrimSpace(noteContent)
+
+			if err != nil {
+				fmt.Println("Error occured while reading input")
+			}
+
+			// Open or create file and write to it
+			file, err := os.OpenFile("notes.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644) // 0644 for the permissoins in linux to give write and read permissions
+			if err != nil {
+				fmt.Println("Error occured while opening file")
+			}
+
+			defer file.Close()
+
+			// actually write to file now
+			if _, err := file.WriteString(noteContent + "\n"); err != nil {
+				fmt.Println("Error occured while writing to file")
+			} else {
+				fmt.Println("Note content written to file")
+			}
+		}
+
 	}
-
-	// Open or create file and write to it
-	file, err :=os.OpenFile("notes.txt",os.O_APPEND | os.O_CREATE | os.O_WRONLY,0644) // 0644 for the permissoins in linux to give write and read permissions
-	if err != nil{
-		fmt.Println("Error occured while opening file")
-	}
-
-	defer file.Close()
-
-	// actually write to file now
-
-	if _,err := file.WriteString(noteContent) ; err !=nil{
-		fmt.Println("Error occured while writing to file")
-	}else{
-		fmt.Println("Note content written to file")
-	}
-	
+	// this is the main closing braces
 }
+
